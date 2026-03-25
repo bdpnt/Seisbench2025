ba4fe741fe4a032bc2a503b30f3b09f08d9ea64a
Basile <dupont.basile@orange.fr>
2026-03-25 10:37:22 +0100

    Added files for zone_Arette test

--- zone_Arette/Arette_obs.py
+++ zone_Arette/Arette_obs.py
@@ -8,6 +8,7 @@ from parameters import Parameters
 # from global_obs import fusion as fusion_obs
 from NLL_run import remove_far_picks as removeFarPicks_nll
 from NLL_run import gen_run_files as genRunFiles_nll
+from NLL_run import clean_post_run as cleanPost_nll
 # import subprocess
 
 # # Fusion all bulletins
@@ -34,25 +35,34 @@ from NLL_run import gen_run_files as genRunFiles_nll
 
 # removeFarPicks_nll.removeFarPicks(params_farpicks)
 
-# Generate run file
-params_run_W = Parameters(
-    fileBulletin = "obs/GLOBAL_200.obs", # GLOBAL OBS file to use
-    fileInventory = '../stations/GLOBAL_inventory.xml', # GLOBAL inventory file to use
-    fileMap = '../stations/GLOBAL_code_mapping.txt', # AlternateCodes map file to use
-    fileBulletinIn = "obs/GLOBAL_W_200.obs", # child OBS events file to generate
-    fileStations = 'stations/GTSRCE_W_200.txt', # GTSRCE stations file to generate
-    fileRunSave = 'run/run_W_200.in', # run file to generate
-    latMin_event = 42.6, # minimum latitude for the event box
-    latMax_event = 43.3, # maximum latitude for the event box
-    lonMin_event = -2.0, # minimum longitude for the event box
-    lonMax_event = 0.5, # maximum longitude for the event box
-    fileModel = 'model/Pyrenees_W_200/Pyrenees_W_200', # model file to generate
-    fileTime = 'time/Pyrenees_W_200/Pyrenees_W_200', # time file to generate
-    fileBulletinOut = 'loc/GLOBAL_W_200/GLOBAL_W_200.obs', # loc file to generate
-    latMin_box = 42.15, # minimum latitude for the main box
-    lonMin_box = -3.2, # minimum longitude for the main box
-    VGGRID = [9000,800], # VGGRID h/v parameters
-    LOCGRID = [8000,3500,800], # LOCGRID E/W ; N/S ; U/D parameters
+# # Generate run file
+# params_run_W = Parameters(
+#     fileBulletin = "obs/GLOBAL_200.obs", # GLOBAL OBS file to use
+#     fileInventory = '../stations/GLOBAL_inventory.xml', # GLOBAL inventory file to use
+#     fileMap = '../stations/GLOBAL_code_mapping.txt', # AlternateCodes map file to use
+#     fileBulletinIn = "obs/GLOBAL_W_200.obs", # child OBS events file to generate
+#     fileStations = 'stations/GTSRCE_W_200.txt', # GTSRCE stations file to generate
+#     fileRunSave = 'run/run_W_200.in', # run file to generate
+#     latMin_event = 42.6, # minimum latitude for the event box
+#     latMax_event = 43.3, # maximum latitude for the event box
+#     lonMin_event = -2.0, # minimum longitude for the event box
+#     lonMax_event = 0.5, # maximum longitude for the event box
+#     fileModel = 'model/Pyrenees_W_200/Pyrenees_W_200', # model file to generate
+#     fileTime = 'time/Pyrenees_W_200/Pyrenees_W_200', # time file to generate
+#     fileBulletinOut = 'loc/GLOBAL_W_200/GLOBAL_W_200.obs', # loc file to generate
+#     latMin_box = 42.15, # minimum latitude for the main box
+#     lonMin_box = -3.2, # minimum longitude for the main box
+#     VGGRID = [9000,800], # VGGRID h/v parameters
+#     LOCGRID = [8000,3500,800], # LOCGRID E/W ; N/S ; U/D parameters
+# )
+
+# genRunFiles_nll.genRun(params_run_W) # Generate the run file
+
+# Clean the files post-run
+params_clean_W = Parameters(
+    folderLoc = 'loc/GLOBAL_W_200',
+    obsFile = 'GLOBAL_W_200.obs',
+    fileBulletin = 'RESULT/GLOBAL_W_200.txt',
 )
 
-genRunFiles_nll.genRun(params_run_W) # Generate the run file
+cleanPost_nll.writeEvents(params_clean_W)
