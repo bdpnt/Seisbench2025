11dfa8c812ce09b3005060874125916052d10cc0
Basile <dupont.basile@orange.fr>
2026-04-03 03:24:50 +0200

    Minor modification to __init__.py, to remove some files that should have been removed previously

--- NLL_run/__init__.py
+++ NLL_run/__init__.py
@@ -5,10 +5,7 @@ __all__ = [
     "remove_far_picks",
     "gen_run_files",
     "clean_post_run",
-    # "map_post_run",
     "gen_second_run_files",
-    "error_maps",
-    "depth_maps",
     # "merge_catalogs",
     "match_catalogs",
 ]
